8bac424e0afb8388aa8935ab29148bb1bb4f60b3
Sumner Evans <me@sumnerevans.com>
2021-08-30 15:37:02 -0600

    ci: example config in Docker container

--- docker-run.sh
+++ docker-run.sh
@@ -9,6 +9,15 @@ function fixperms {
 	chown -R $UID:$GID /data /opt/chatwoot
 }
 
+if [[ ! -f /data/config.yaml ]]; then
+	cp /opt/chatwoot/config.sample.json /data/config.json
+	echo "Didn't find a config file."
+	echo "Copied default config file to /data/config.json"
+	echo "Modify that config file to your liking."
+	echo "Start the container again after that to generate the registration file."
+	exit
+fi
+
 cd /data
 fixperms
 exec su-exec $UID:$GID /usr/bin/chatwoot
