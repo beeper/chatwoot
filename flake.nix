19a863a1696ecbbe36b3b9046528d145ce9eba66
Sumner Evans <me@sumnerevans.com>
2023-12-14 08:10:16 -0700

    flake: update lockfile and add default package
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -7,31 +7,23 @@
   };
 
   outputs = { self, nixpkgs-unstable, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs-unstable { system = system; };
-        in
-        {
-          packages.chatwoot = pkgs.buildGoModule rec {
-            pname = "chatwoot";
-            version = "unstable-2023-05-20";
+    (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs-unstable { system = system; };
+      in rec {
+        packages.chatwoot = pkgs.buildGoModule {
+          pname = "chatwoot";
+          version = "unstable-2023-12-14";
 
-            src = ./.;
+          src = ./.;
 
-            propagatedBuildInputs = [ pkgs.olm ];
+          propagatedBuildInputs = [ pkgs.olm ];
 
-            vendorSha256 = "sha256-whKiWndBv4TX/H19sINU75XK+PmdMomNkZpY84rDAbk=";
-          };
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              go_1_21
-              olm
-              pre-commit
-              gotools
-              gopls
-            ];
-          };
-        }
-      ));
+          vendorHash = "sha256-MYmJf8UlR6IhF5SVEQ9nap1vWFH00TjFjKrmISInXMg=";
+        };
+        packages.default = packages.chatwoot;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ go_1_21 olm pre-commit gotools gopls ];
+        };
+      }));
 }
